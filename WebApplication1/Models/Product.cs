365a26536be06099ea3bfe75ead175c72d60e79c
Krishna Kumar <kumarkrishna52088@gmail.com>
2024-04-20 19:54:30 +0530

    Added Project Flies at the first time

---
+++ WebApplication1/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Price { get; set; }
+        public string? Description { get; set; }
+        public List<Cart>? carts { get; set; }
+        public List<WishList>? wishLists { get; set; }
+
+        [ForeignKey("category")]
+        public int? Category_Id { get; set; }
+
+        public Category? category { set; get; }
+    }
+}
